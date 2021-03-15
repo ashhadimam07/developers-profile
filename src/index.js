fffa7dfeb9e0f880344e3c9f9f67db0c0b5e9b7e
ashhadimam07 <ashhadimam07@gmail.com>
2021-03-15 14:09:10 +0530

    Create dev-profile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DeveloperProfile from './pages/DeveloperProfile';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function App() {
+  return (
+      <Router>
+          <Switch>
+              <Route exact path='/'  component={HomePage} />
+              <Route path='/api/developers/:developerId' exact component={DeveloperProfile} />
+          </Switch>
+      </Router>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
